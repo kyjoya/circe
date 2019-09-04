85af78b336e37607ed35d8fe051733e31b30f904
Kylee Acker <kyleejacker@gmail.com>
2019-09-04 17:55:20 -0400

    Deployment

--- circe-static/gatsby-config.js
+++ circe-static/gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Circe`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `kylee`,
     siteUrl: process.env.SITE_URL
   },
   plugins: [
